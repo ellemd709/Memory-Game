bd4b3e11bc381b8504eb1717e38604661e3fde60
ellemd709 <ellemd@icloud.com>
2022-07-27 14:31:45 -0230

    const added js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let greet = "Hello Everyone";
 console.log(greet);
+
+const selectors = {
+    boardContainer: document.querySelector('.table-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
