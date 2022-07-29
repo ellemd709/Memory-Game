8cc35ce2759bf09dbad77fd40ce7724ac95894c8
ellemd709 <ellemd@icloud.com>
2022-07-29 11:53:09 -0230

    style edits

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ const selectors = {
     win: document.querySelector('.win')
 }
 
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
 const startGame = () => {
     state.gameStarted = true
     selectors.start.classList.add('disabled')
