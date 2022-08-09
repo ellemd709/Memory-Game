8874fe719436389e2213b1b1920c2fa42e08674a
ellemd709 <ellemd@icloud.com>
2022-08-09 12:37:28 -0230

    flipping cards

--- script.js
+++ script.js
@@ -98,3 +98,30 @@ for (var i = 0; i < selectors.cards.length; i++) {
     })
 }
 
+//flipping
+
+const flipCard = card => {
+    state.flippedCards++
+    state.totalFlips++
+
+    if (!state.gameStarted) {
+        startGame()
+    }
+
+    if (state.flippedCards <= 2) {
+        card.classList.add('flipped')
+    }
+
+    if (state.flippedCards === 2) {
+        const flippedCards = document.querySelectorAll('.flipped:not(.matched)')
+
+        if (flippedCards[0].innerText === flippedCards[1].innerText) {
+            flippedCards[0].classList.add('matched')
+            flippedCards[1].classList.add('matched')
+        }
+
+        setTimeout(() => {
+            flipBackCards()
+        }, 1000)
+    }
+}
