65dc22085dafacc8d792984ff0811b5109217a37
ellemd709 <ellemd@icloud.com>
2022-08-19 10:06:59 -0230

    js clean up

--- script.js
+++ script.js
@@ -54,144 +54,126 @@ const state = {
         return array;
       }
     
-    const pickRandom = (array, items) => {
-        const clonedArray = [...array];
-        const randomPicks = [];
-    
-        for (let index = 0; index < items; index++) {
-            const randomIndex = Math.floor(Math.random() * clonedArray.length);
-    
-            randomPicks.push(clonedArray[randomIndex]);
-            clonedArray.splice(randomIndex, 1);
-        }
-    
-        return randomPicks;
-    };
-    
     // starts game and keeps stats
-    const startGame = () => {
-        state.gameStarted = true;
-        selectors.start.classList.add("disabled");
+const startGame = () => {
+    state.gameStarted = true;
+    selectors.start.classList.add("disabled");
+  
+    state.loop = setInterval(() => {
+      state.totalTime++;
+  
+      selectors.moves.innerText = `${state.totalFlips} moves`;
+      selectors.timer.innerText = `time: ${state.totalTime} sec`;
+    }, 1000);
+  };
+  
+  // onclick event for button to start game
+  selectors.start.onclick = startGame;
+  
+  // resets cards if a pair doesn't match
+  const flipBackCards = () => {
+    document.querySelectorAll(".card:not(.matched)").forEach((card) => {
+      card.classList.remove("flipped");
+    });
+  
+    state.flippedCards = 0;
+  };
+  
+  // generate board
+  function generateGame() {
+    state.picks = ["card 1", "card 2", "card 3", "card 4", "card 5", "card 6", "card 7", "card 8", "card 9", "card 10", "card 11", "card 12"];
+    const items = shuffle([...state.picks]);
     
-        state.loop = setInterval(() => {
-            state.totalTime++;
-    
-            selectors.moves.innerText = `${state.totalFlips} moves`;
-            selectors.timer.innerText = `time: ${state.totalTime} sec`;
-        }, 1000);
-    };
-    
-    // onclick event for button to start game
-    selectors.start.onclick = startGame;
-    
-    // resets cards if a pair doesn't match
-    const flipBackCards = () => {
-        document.querySelectorAll(".card:not(.matched)").forEach((card) => {
-            card.classList.remove("flipped");
-        });
-    
-        state.flippedCards = 0;
-    };
-    
-    // generate board
-    
-    function generateGame() {
-        const card = ["card 1", "card 2", "card 3", "card 4", "card 5", "card 6"];
-        state.picks = pickRandom(card, 6);
-        const items = shuffle([...state.picks, ...state.picks]);
-        for (var i = 0; i < items.length; i++) {
-            selectors.cards[i].innerHTML = `
-        <div class="card-front"></div>
-                            <div class="card-back" style="background-image: url('${card_fronts[items[i]]
-                }')">${items[i]}
-                        </div>`;
-                        selectors.cards[i].class
-        }
+    // loop through the cards and add an inline image for the card-back
+    for (var i = 0; i < items.length; i++) {
+      selectors.cards[i].setAttribute("style", "background-image: url("+`${card_fronts[items[i]]}`+"");
     }
-    
-    function flipCard(card) {
-        state.flippedCards++;
-        state.totalFlips++;
-    
-        if (!state.gameStarted) {
-            startGame();
-        }
-    
-        if (state.flippedCards <= 2) {
-            card.classList.add("flipped");
-        }
-    
-        if (state.flippedCards === 2) {
-            const flippedCards = document.querySelectorAll(".flipped:not(.matched)");
-    
-            if (flippedCards[0].innerText === flippedCards[1].innerText) {
-                flippedCards[0].classList.add("matched");
-                flippedCards[1].classList.add("matched");
-            }
-    
-            setTimeout(() => {
-                flipBackCards();
-            }, 1000);
-        }
-    
-        if (!document.querySelectorAll(".card:not(.flipped)").length) {
-            setTimeout(() => {
-                selectors.boardContainer.classList.add("flipped");
-                selectors.win.innerHTML = `
-                    <span class="win-text">
-                        You won!<br />
-                        with <span class="highlight">${state.totalFlips}</span> moves<br />
-                        under <span class="highlight">${state.totalTime}</span> seconds
-                        <button class="replay">Play again</button>
-                    </span>
-                `;
-                clearInterval(state.loop);
-            }, 1000);
-        }
+  }
+  
+  function flipCard(card) {
+    state.flippedCards++;
+    state.totalFlips++;
+  
+    if (!state.gameStarted) {
+      startGame();
     }
-    
-    const attachEventListeners = () => {
-        document.addEventListener("click", (event) => {
-            const eventTarget = event.target;
-            const eventParent = eventTarget.parentElement;
-    
-            if (
-                eventTarget.className.includes("card") &&
-                !eventParent.className.includes("flipped")
-            ) {
-                flipCard(eventParent);
-            } else if (
-                eventTarget.nodeName === "BUTTON" &&
-                !eventTarget.className.includes("disabled")
-            ) {
-                startGame();
-            }
-    
-            if (eventTarget.className.includes("replay")) {
-                selectors.boardContainer.classList.remove("flipped");
-                document.querySelectorAll(".card").forEach((card) => {
-                    card.classList.remove("flipped", "matched");
-                });
-                replayGame();
-            }
-        });
-    };
-
-    // a replay function to play again
-    
-    const replayGame = () => {
-        state.gameStarted = false;
-        state.flippedCards = 0;
-        state.totalFlips = 0;
-        state.totalTime = 0;
+  
+    if (state.flippedCards <= 2) {
+      card.classList.add("flipped");
+    }
+  
+    if (state.flippedCards === 2) {
+      const flippedCards = document.querySelectorAll(".flipped:not(.matched)");
+  
+      // if the card-back background-images are the same on flipped cards, we add a class matched
+      if (flippedCards[0].innerHTML === flippedCards[1].innerHTML) {
+        flippedCards[0].classList.add("matched");
+        flippedCards[1].classList.add("matched");
+      }
+  
+      // if they aren't, flip back cards
+      setTimeout(() => {
+        flipBackCards();
+      }, 1000);
+    }
+  
+    if (!document.querySelectorAll(".card:not(.flipped)").length) {
+      setTimeout(() => {
+        selectors.boardContainer.classList.add("flipped");
+        selectors.win.innerHTML = `
+                  <span class="win-text">
+                      You won!<br />
+                      with <span class="highlight">${state.totalFlips}</span> moves<br />
+                      under <span class="highlight">${state.totalTime}</span> seconds
+                      <button class="replay">Play again</button>
+                  </span>
+              `;
         clearInterval(state.loop);
-        state.loop = null;
-        state.picks = null;
-        generateGame();
-        selectors.moves.innerText = `${state.totalFlips} moves`;
-        selectors.timer.innerText = `time: ${state.totalTime} sec`;
-        selectors.start.classList.remove("disabled");
-    };
-    
+      }, 1000);
+    }
+  }
+  
+  const attachEventListeners = () => {
+    document.addEventListener("click", (event) => {
+      const eventTarget = event.target;
+      const eventParent = eventTarget.parentElement;
+  
+      if (
+        eventTarget.className.includes("card") &&
+        !eventParent.className.includes("flipped")
+      ) {
+        flipCard(eventParent);
+      } else if (
+        eventTarget.nodeName === "BUTTON" &&
+        !eventTarget.className.includes("disabled")
+      ) {
+        startGame();
+      }
+      // if replay button is clickd, rebuild the game
+      if (eventTarget.className.includes("replay")) {
+        selectors.boardContainer.classList.remove("flipped");
+        document.querySelectorAll(".card").forEach((card) => {
+          card.classList.remove("flipped", "matched");
+        });
+        replayGame();
+      }
+    });
+  };
+  
+  // reset states and generate a new game
+  const replayGame = () => {
+    state.gameStarted = false;
+    state.flippedCards = 0;
+    state.totalFlips = 0;
+    state.totalTime = 0;
+    clearInterval(state.loop);
+    state.loop = null;
+    state.picks = null;
     generateGame();
-    attachEventListeners();
+    selectors.moves.innerText = `${state.totalFlips} moves`;
+    selectors.timer.innerText = `time: ${state.totalTime} sec`;
+    selectors.start.classList.remove("disabled");
+  };
+  
+  generateGame();
+  attachEventListeners();
